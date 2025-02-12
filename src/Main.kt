6a076f529502f850b05b0c3c53785d6d19243074
azmenezi <azmenezi@gmail.com>
2025-02-12 19:04:31 +0300

    task 4 done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    greetSeaCreatures()
+    println(findPearl(6))
+    println(divideTreasure(1000.0, 5))
+    println(calculateDepth(1500,800))
 }
+
+val greetSeaCreatures = {
+    println("hello, deep sea")
+}
+
+val findPearl = { numberOfPearls: Int -> numberOfPearls * numberOfPearls}
+
+val divideTreasure = {  totalTreasuresKWD: Double, numberOfExplorers: Int -> totalTreasuresKWD / numberOfExplorers}
+
+val calculateDepth: (Int, Int) -> Int = { location1,location2 -> location1 - location2 }
